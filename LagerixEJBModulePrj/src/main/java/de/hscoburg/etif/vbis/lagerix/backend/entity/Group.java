aa1ca3e9ed57f09c220742be51b4be6a9f4c404d
zuch1000 <zuch1000@hs-coburg.de>
2013-12-06 19:21:38 +0100

    removed benutzer benutzer

--- LagerixEJBModulePrj/src/main/java/de/hscoburg/etif/vbis/lagerix/backend/entity/Group.java
+++ LagerixEJBModulePrj/src/main/java/de/hscoburg/etif/vbis/lagerix/backend/entity/Group.java
@@ -1,11 +1,11 @@
 package de.hscoburg.etif.vbis.lagerix.backend.entity;
- 
+
 /**
- * 
+ *
  * @author zuch1000
  */
 public enum Group {
-     
-    ADMINISTRATOR, BENUTZER,LAGERARBEITER,EINKAEUFER,LAGERVERWALTER;
-     
+
+    ADMINISTRATOR, LAGERARBEITER, EINKAEUFER, LAGERVERWALTER;
+
 }
--- LagerixEJBRemoteInterfacePrj/src/main/java/de/hscoburg/etif/vbis/lagerix/backend/interfaces/dto/base/GroupType.java
+++ LagerixEJBRemoteInterfacePrj/src/main/java/de/hscoburg/etif/vbis/lagerix/backend/interfaces/dto/base/GroupType.java
@@ -1,11 +1,11 @@
 package de.hscoburg.etif.vbis.lagerix.backend.interfaces.dto.base;
- 
+
 /**
- * 
+ *
  * @author zuch1000
  */
 public enum GroupType {
-     
-    ADMINISTRATOR, BENUTZER,LAGERARBEITER,EINKAEUFER,LAGERVERWALTER;
-     
+
+    ADMINISTRATOR, LAGERARBEITER, EINKAEUFER, LAGERVERWALTER;
+
 }
